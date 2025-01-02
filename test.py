e5ad4ecefcca8cc41f954bbb5ef447ce6ef27a54
lily <35861708+lhl2211000@users.noreply.github.com>
2025-01-02 10:41:25 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("CUDA可用")
-    print(f"当前CUDA设备数量: {torch.cuda.device_count()}")
-    print(f"当前CUDA设备名称: {torch.cuda.get_device_name(0)}")
-else:
-    print("CUDA不可用")
